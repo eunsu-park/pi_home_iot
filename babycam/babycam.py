6595292f713ee29a3960f5e0d7c0cda771ad4b79
Eunsu Park <phd.choux@gmail.com>
2024-08-21 23:50:47 +0900

    Update babycam.py

--- babycam/babycam.py
+++ babycam/babycam.py
@@ -1,43 +1,38 @@
+import cv2
 from flask import Flask, render_template, Response
-from picamera2 import Picamera2
-
+from picamera2 import Picamera2, Preview
 
 app = Flask(__name__)
 camera = Picamera2()
 
-
 camera.configure(camera.create_preview_configuration(main={"size": (640, 480)}))
 camera.start()
-
 
 def gen_frames():
     while True:
         frame = camera.capture_array()
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
-
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route('/set_brightness/<int:brightness>')
 def set_brightness(brightness):
     camera.set_controls({"Brightness": brightness})
     return f"Brightness set to {brightness}"
 
-
 @app.route('/set_contrast/<int:contrast>')
 def set_contrast(contrast):
     camera.set_controls({"Contrast": contrast})
     return f"Contrast set to {contrast}"
-
 
 if __name__ == '__main__':
     import argparse
